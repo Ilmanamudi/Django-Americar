0efdd0beda711224b5518bd8a235126e1dcaabe9
marisa <ilmanamudi@gmail.com>
2022-12-15 17:56:36 -0300

    upgrade

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ pip install -r requirements.txt
 #pip install --upgrade pip
 # pip install --force-reinstall -U setuptools
 
-python americar/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 python manage.py migrate
